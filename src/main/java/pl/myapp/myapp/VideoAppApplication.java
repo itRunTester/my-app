671e3190e7ed72df0d78f598f57829c179045b92
itRunTester <itrun.tester@gmail.com>
2020-09-26 13:21:21 +0200

    package

--- src/main/java/pl/myapp/myapp/VideoAppApplication.java
+++ src/main/java/pl/myapp/myapp/VideoAppApplication.java
@@ -1,4 +1,4 @@
-package pl.myapp.myapp.spring.boot.controller;
+package pl.myapp.myapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/pl/myapp/myapp/MyAppApplicationTests.java
+++ src/test/java/pl/myapp/myapp/MyAppApplicationTests.java
@@ -1,4 +1,4 @@
-package pl.myapp.myapp.spring.boot.controller;
+package pl.myapp.myapp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,5 +9,4 @@ class MyAppApplicationTests {
     @Test
     void contextLoads() {
     }
-
 }
